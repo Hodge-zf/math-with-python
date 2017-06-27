9ca229da425b5d4d112a3c4299f84a1f6e6e99a5
Hodge-zf <xx.skcs@gmail.com>
2017-06-27 17:48:14 -0500

    examples converted to callable functions

--- SimpleArithmetic.py
+++ SimpleArithmetic.py
@@ -1,39 +1,25 @@
 class SimpleArithmetic:
 
+	def simple_add(x,y):
+		print(x+y)
 
-	def SimpleAdd(x,y):
-		return x+y
+	def simple_subtract(x,y):
+		print(x-y)
 
+	def simple_multiply(x,y):
+		print(x*y)
 
-	def SimpleSubtract(x,y):
-		return x-y
+	def simple_divide(x,y):
+		print(x/y)
 
-
-	def SimpleMultiply(x,y):
-		return x*y
-
-
-	def SimpleDivide(x,y):
-		return x/y
-
-
-	def FloorDivide(x,y):
+	def floor_divide(x,y):
 		#the double-slash operator performs division and then rounds the result down to the next-lowest integer
 		#this provides interesting results when combined with negative products
-		return x//y
+		print(x//y)
 
+	def simple_modulus(x,y):
+		print(x%y)
 
-	def SimpleModulus(x,y):
-		return x%y
+	def simple_power(x,y):
+		print(x**y)
 
-
-	def SimplePower(x,y):
-		return x**y
-
-	print(SimpleAdd(1,2))
-	print(SimpleSubtract(2,1))
-	print(SimpleMultiply(1.5,3.5))
-	print(SimpleDivide(10,3))
-	print(FloorDivide(-3,2))
-	print(SimpleModulus(9,2))
-	print(SimplePower(8,2))
