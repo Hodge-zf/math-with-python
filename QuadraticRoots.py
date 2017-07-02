4465e84cbb5c9c236ce0d36362aee19c439e6363
Hodge-zf <xx.skcs@gmail.com>
2017-07-02 13:42:04 -0500

    Quadratic Root Calculation

---
+++ QuadraticRoots.py
@@ -0,0 +1,16 @@
+class QuadraticRoots:
+
+	def find_roots(a, b, c):
+
+		discriminant = (b*b - 4*a*c)**0.5
+		root_1 = (-b + discriminant)/(2*a)
+		root_2 = (-b - discriminant) / (2 * a)
+
+		print('Root 1: {0}'.format(root_1))
+		print('Root 2: {0}'.format(root_2))
+		
+	if __name__ == '__main__':
+		a = input('Enter a: ')
+		b = input('Enter b: ')
+		c = input('Enter c: ')
+		find_roots(float(a), float(b), float(c))
