6ed6734612b97c9e8ad402f8e46f5086dd084ac1
Hodge-zf <xx.skcs@gmail.com>
2017-07-26 13:17:42 -0500

    Fitting circles in a square challenge complete

--- Chapter6/CirclesInASquare.py
+++ Chapter6/CirclesInASquare.py
@@ -1,10 +1,29 @@
 from matplotlib import pyplot as plt
 
+
 def draw_square():
 	ax = plt.axes(xlim = (0, 6), ylim = (0, 6))
 	square = plt.Polygon([(1, 1), (5, 1), (5, 5), (1, 5)], closed = True)
 	ax.add_patch(square)
-	plt.show()
+
+
+
+def create_circle(x, y):
+	circle = plt.Circle((x, y), radius = 0.5, color = 'white')
+	return circle
+
+def draw_circle(circle):
+	ax = plt.gca()
+	ax.add_patch(circle)
+	plt.axis('scaled')
 
 if __name__ == '__main__':
 	draw_square()
+	y = 1.5
+	while y < 5:
+		x = 1.5
+		while x < 5:
+			c = draw_circle(create_circle(x, y))
+			x += 1.0
+		y+= 1.0
+	plt.show()
