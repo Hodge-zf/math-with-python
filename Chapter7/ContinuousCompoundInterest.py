cca347fe482f2553d96faf8cc9592e22169f58f6
Hodge-zf <xx.skcs@gmail.com>
2018-12-19 20:03:50 -0600

    Compound interest formula

--- Chapter7/ContinuousCompoundInterest.py
+++ Chapter7/ContinuousCompoundInterest.py
@@ -3,3 +3,8 @@ from sympy import Limit, Symbol, S
 n = Symbol('n')
 print(Limit((1+1/n)**n, n, S.Infinity).doit())
 
+p = Symbol('p', positive=True)
+r = Symbol('r', positive=True)
+t = Symbol('t', positive=True)
+
+print(Limit(p*(1+r/n)**(n*t), n, S.Infinity).doit())
